cf78048aafed1bedf7ed3879b5e46f07f6c1c02e
div-wang <gh110827@gmail.com>
2015-08-16 14:47:54 +0800

    frist commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# div-wang的博客
+
+div-wang的技术随想录😄
+
+## 如何开始
+
+请修改 `src` 目录里的对应文件
+
+* 本地有 Node.JS 和 Python 2.7 环境;  
+* 全局安装了 gulp，如果没有请 `npm i -g gulp`;  
+* 克隆本项目`git clone https://github.com/div-wang/div-wang.github.io.git`; 
+* 进入项目目录执行 `npm install`;   
+* 终端输入 `gulp` 即可开始  
+
+Project page: https://div-wang.github.io/ 
+
+## License
+
+本项目由 div-wang 版权所有©
+
+
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  swig = require('gulp-swig'),
+  serve = require('gulp-serve')
+  markdown = require('gulp-markdown');
+
+gulp.task('serve', serve({
+  port: '3000',
+  root: ['.']
+}))
+
+gulp.task('css', function() {
+  gulp.src('./src/scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'))
+})
+
+gulp.task('html', function() {
+  var time = new Date().getTime()
+  gulp.src('./src/html/index.html')
+    .pipe(swig({
+      defaults: {
+        cache: false,
+        locals: {
+          time: time
+        }
+      }
+    }))
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('blog', function() {
+  var time = new Date().getTime()
+  gulp.src('./src/blog/index.html')
+    .pipe(swig({
+      defaults: {
+        cache: false,
+        locals: {
+          time: time
+        }
+      }
+    }))
+    .pipe(gulp.dest('./blog'))
+})
+
+
+gulp.task('md', function () {
+    gulp.src('./src/blog/*.md')
+      .pipe(markdown())
+      .pipe(gulp.dest('./blog'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/scss/*.scss', ['css'])
+  gulp.watch('./src/html/*.html', ['html'])
+  gulp.watch('./src/blog/*.md', ['html'])
+})
+
+gulp.task('build', ['css', 'blog', 'html'])
+
+gulp.task('default', ['build', 'watch', 'serve'])
