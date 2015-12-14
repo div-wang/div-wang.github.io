a739b2406af0f94f00e42be0252b33df2f255595
wangguangui <826950544@qq.com>
2015-12-14 16:01:36 +0800

    临时提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,61 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  swig = require('gulp-swig'),
-  serve = require('gulp-serve'),
-  markdown = require('gulp-markdown'),
-  markdownDocs = require('gulp-markdown-docs');
+    sass = require('gulp-sass'),
+    swig = require('gulp-swig'),
+    serve = require('gulp-serve'),
+    markdown = require('gulp-markdown'),
+    markdownDocs = require('gulp-markdown-docs');
 
 gulp.task('serve', serve({
-  port: '3000',
-  root: ['./div-wang']
+    port: '3000',
+    root: ['./']
 }))
 
 gulp.task('css', function() {
-  gulp.src('./src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./div-wang/static/css'))
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./static/css'))
 })
 
 gulp.task('html', function() {
-  var time = new Date().getTime()
-  gulp.src('./src/html/index.html')
-    .pipe(swig({
-      defaults: {
-        cache: false,
-        locals: {
-          time: time,
-          blog: false
-        }
-      }
-    }))
-    .pipe(gulp.dest('./div-wang'))
+    var time = new Date().getTime()
+    gulp.src('./src/html/index.html')
+        .pipe(swig({
+            defaults: {
+                cache: false,
+                locals: {
+                    time: time,
+                    blog: false
+                }
+            }
+        }))
+        .pipe(gulp.dest('./'))
 })
 
 gulp.task('blog', function() {
-  var time = new Date().getTime()
-  gulp.src('./src/html/blog/*.html')
-    .pipe(swig({
-      defaults: {
-        cache: false,
-        locals: {
-          time: time,
-          blog: true
-        }
-      }
-    }))
-    .pipe(gulp.dest('./div-wang/blog'))
+    var time = new Date().getTime()
+    gulp.src('./src/html/blog/*.html')
+        .pipe(swig({
+            defaults: {
+                cache: false,
+                locals: {
+                    time: time,
+                    blog: true
+                }
+            }
+        }))
+        .pipe(gulp.dest('./blog'))
 })
 
-gulp.task('md', function () {
+gulp.task('md', function() {
     gulp.src('./src/blog/*.md')
-      .pipe(markdown())
-      .pipe(gulp.dest('./blog'));
+        .pipe(markdown())
+        .pipe(gulp.dest('./blog'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/scss/*.scss', ['css'])
-  gulp.watch('./src/html/*.html', ['html'])
-  gulp.watch('./src/blog/*.md', ['html'])
+    gulp.watch('./src/scss/*.scss', ['css'])
+    gulp.watch('./src/html/*.html', ['html'])
+    gulp.watch('./src/blog/*.md', ['md'])
 })
 
 gulp.task('build', ['css', 'blog', 'html', 'md'])
